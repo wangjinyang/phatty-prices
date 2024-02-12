8eb0f2950217973d040a59dd206b214f23b007e7
Tom <214997474@qq.com>
2024-02-12 09:51:26 +0800

    chore: add vercel.json

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,17 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { updatePricesJob } from "../../cron"
+import { updatePricesJob } from "../../cron";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    await updatePricesJob()
+    if (
+      req.headers["Authorization"] !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      console.log('Unauthorized');
+      return res.status(401).end("Unauthorized");
+    }
+    console.log('updatePricesJob');
+    await updatePricesJob();
     return res.status(200).json({ message: "success" });
   } catch (err) {
     console.log("err: ", err);
     res.status(500).json({ message: "server internal error!" });
   }
 }
-
